9262e9840538b5ff91a9108b1a7b7fb5f351b9ef
Daria <daria34522@gmail.com>
2026-04-07 13:57:15 +0200

    Dodanie getTotal

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,8 +2,8 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    private int value;
-    private Node prev, next;
+    public int value;
+    public Node prev, next;
 
     public Node(int i) {
         value = i;
